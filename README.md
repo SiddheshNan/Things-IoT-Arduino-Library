f3effc319acc674c3bc211a2d07ef03d0daf2d8b
Siddhesh <37670217+SiddheshNan@users.noreply.github.com>
2020-06-23 19:19:04 +0530

    v1 - initial release

---
+++ README.md
@@ -0,0 +1,17 @@
+# Things IoT Arduino Library
+
+The Things IoT Arduino Client Library is an easy to use client library to connect your IoT devices to the 
+[Things IoT](https://things-iot.siddhesh.me "Things IoT Cloud Platform") Cloud IoT platform. This is a library specifically designed for the Arduino IDE, so you can easily install it in your environment and start connecting your devices within minutes. It supports multiple network interfaces like Wifi. So you can use it in several devices like Any ESP8266 variant like NodeMCU, etc.
+
+It requires modern Arduino IDE version, starting at 1.6.3.
+
+## Documentation
+
+Please, refer to the following pages for the full documentation of the Arduino Client Library & Things IoT Cloud Platform.
+
+[Arduino Client Library Documentation](https://things-iot.siddhesh.me/arduino-docs)
+
+[Things IoT Cloud Platform Documentation](https://things-iot.siddhesh.me/cloud-docs)
+
+## License
+GNU GENERAL PUBLIC LICENSE v2
---
+++ example/things-iot-example/things-iot-example.ino
@@ -0,0 +1,102 @@
+#include <ESP8266WiFi.h>
+#include <things8266.h>
+
+
+//------------------------
+String HandleDataResponse(String res, String type, String value);
+String Things8266::HandleData(String res, String type, String value) {
+    return HandleDataResponse(res, type, value);
+}
+Things8266 DeviceClient;
+WiFiClient espClient;
+PubSubClient client(espClient);
+//--------------------------
+
+
+
+
+int LED = 2;
+
+String testString;
+String placeholderString = "yui";
+
+
+
+void setup()
+{
+
+    Serial.begin(115200);
+    DynamicJsonDocument thing(1024);
+    thing["action"] = "create_resources";
+
+
+
+    // Set Output Pins
+
+    pinMode(LED, OUTPUT);
+
+    //---------------
+
+
+    // SetUp Device
+
+    DeviceClient.SetHost("things-iot.siddhesh.me");
+
+    DeviceClient.SetDevice("username", "device", "pass");
+
+    DeviceClient.SetWiFi("ssid", "pass");
+
+
+
+
+    // CREATE RESOURCES HERE
+
+
+    // Kind: switch
+    // Type: input-output we can view it and modify it from cloud
+
+    thing["resources"]["led"]["type"] = "io";
+    thing["resources"]["led"]["kind"] = "switch";
+
+
+    // Kind: String / Value ( 2 way )
+    // Type: input-output we can view it and modify it from cloud
+
+    thing["resources"]["placeholder"]["type"] = "io";
+    thing["resources"]["placeholder"]["kind"] = "value";
+
+
+    // Kind: String / Value ( 1 way )
+    // Type Output Only! (we cannot modify it from cloud) [only view it from cloud]
+
+    thing["resources"]["temp"]["type"] = "out";
+    thing["resources"]["temp"]["kind"] = "value";
+
+
+
+    DeviceClient.initDevice(client, thing);
+}
+
+
+
+String HandleDataResponse(String res, String type, String value)
+{
+
+    if (res == "led") return DeviceClient.digitalPin(LED, type, value);
+
+
+    if (res == "temp") return DeviceClient.outputValue(testString);
+
+
+    if (res == "placeholder") return DeviceClient.inputValue(placeholderString, type, value);
+
+}
+
+
+
+void loop()
+{
+    testString = String(millis());
+
+    DeviceClient.DataHandle();
+}
