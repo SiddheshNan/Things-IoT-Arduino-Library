a23c8493677e6f2764b511d84dca65ce87fc078a
Siddhesh <37670217+SiddheshNan@users.noreply.github.com>
2020-07-17 19:00:07 +0530

    update example

--- examples/things-iot-example/things-iot-example.ino
+++ examples/things-iot-example/things-iot-example.ino
@@ -1,23 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <things8266.h>
 
-
-// Boilerplate code 
-//------------------------
-String HandleDataResponse(String res, String type, String value);
-String Things8266::HandleData(String res, String type, String value) {
-    return HandleDataResponse(res, type, value);
-}
-
-
-
-
 Things8266 DeviceClient;
-WiFiClient espClient;
-PubSubClient client(espClient);
-//--------------------------
-
-
 
 
 int LED = 2;
@@ -25,61 +9,48 @@ int LED = 2;
 String testString;
 String placeholderString = "this is string";
 
-
-
 void setup()
 {
 
-    Serial.begin(115200);
-    DynamicJsonDocument thing(1024);
-    thing["action"] = "create_resources";
+  Serial.begin(115200);
+  DynamicJsonDocument thing(1024);
 
 
 
-    // Set Output Pins
+  // Set Output Pins
 
-    pinMode(LED, OUTPUT);
-
-    //---------------
+  pinMode(LED, OUTPUT);
 
 
-    // SetUp Device
+  // SetUp Device
 
-    DeviceClient.SetHost("things-iot.siddhesh.me");
+  DeviceClient.SetHost("things-iot.siddhesh.me");
 
-    DeviceClient.SetDevice("username", "device-id", "device-pass");
+  DeviceClient.SetDevice("username", "device-id", "device-pass");
 
-    DeviceClient.SetWiFi("ssid", "ssid-pass");
-
-
+  DeviceClient.SetWiFi("ssid", "ssid-pass");
 
 
-    // CREATE RESOURCES HERE
+  // CREATE RESOURCES HERE
+
+  // Kind: switch
+  // Type: input-output we can view it and modify it from cloud
+
+  DeviceClient.CreateResource(thing, "switch", "led");
+
+  // Kind: String / Value ( 2 way )
+  // Type: input-output we can view it and modify it from cloud
+
+  DeviceClient.CreateResource(thing, "1-way", "temp");
 
 
-    // Kind: switch
-    // Type: input-output we can view it and modify it from cloud
+  // Kind: String / Value ( 1 way )
+  // Type Output Only! (we cannot modify it from cloud) [only view it from cloud]
 
-    thing["resources"]["led"]["type"] = "io";
-    thing["resources"]["led"]["kind"] = "switch";
-
-
-    // Kind: String / Value ( 2 way )
-    // Type: input-output we can view it and modify it from cloud
-
-    thing["resources"]["placeholder"]["type"] = "io";
-    thing["resources"]["placeholder"]["kind"] = "value";
+  DeviceClient.CreateResource(thing, "2-way", "placeholder");
 
 
-    // Kind: String / Value ( 1 way )
-    // Type Output Only! (we cannot modify it from cloud) [only view it from cloud]
-
-    thing["resources"]["temp"]["type"] = "out";
-    thing["resources"]["temp"]["kind"] = "value";
-
-
-
-    DeviceClient.initDevice(client, thing);
+  DeviceClient.initDevice(thing);
 }
 
 
@@ -87,13 +58,13 @@ void setup()
 String HandleDataResponse(String res, String type, String value)
 {
 
-    if (res == "led") return DeviceClient.digitalPin(LED, type, value);
+  if (res == "led") return DeviceClient.digitalPin(LED, type, value);
 
 
-    if (res == "temp") return DeviceClient.outputValue(testString);
+  if (res == "temp") return DeviceClient.outputValue(testString);
 
 
-    if (res == "placeholder") return DeviceClient.inputValue(placeholderString, type, value);
+  if (res == "placeholder") return DeviceClient.inputValue(placeholderString, type, value);
 
 }
 
@@ -101,7 +72,7 @@ String HandleDataResponse(String res, String type, String value)
 
 void loop()
 {
-    testString = String(millis());
+  testString = String(millis());
 
-    DeviceClient.DataHandle();
+  DeviceClient.DataHandle();
 }
